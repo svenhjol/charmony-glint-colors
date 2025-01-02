c8ad91f191e032b8162389f770c6bb0cfc386fe0
Sven H <sven.hjol@protonmail.com>
2025-01-02 22:05:55 +0000

    Refactor for Charmony 1.19.0. 1.1.0

--- src/main/java/svenhjol/charmony/glint_colors/common/features/glint_color_templates/Advancements.java
+++ src/main/java/svenhjol/charmony/glint_colors/common/features/glint_color_templates/Advancements.java
@@ -2,7 +2,7 @@ package svenhjol.charmony.glint_colors.common.features.glint_color_templates;
 
 import net.minecraft.world.entity.player.Player;
 import svenhjol.charmony.core.base.Setup;
-import svenhjol.charmony.core.helper.AdvancementHelper;
+import svenhjol.charmony.core.helpers.AdvancementHelper;
 
 public final class Advancements extends Setup<GlintColorTemplates> {
     public Advancements(GlintColorTemplates feature) {
