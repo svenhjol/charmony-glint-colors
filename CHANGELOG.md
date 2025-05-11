a118ad1de7cfcb861e548eaa7ec41230aa853fca
Sven H <sven.hjol@protonmail.com>
2025-05-11 21:38:32 +0000

    Wire up API implementations. 1.6.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # Changelog
 
+## 1.6.1
+
+- Wire up 
+
 ## 1.6.0
 
 - Update for API 1.20
--- src/main/java/svenhjol/charmony/glint_colors/common/features/glint_colors/Providers.java
+++ src/main/java/svenhjol/charmony/glint_colors/common/features/glint_colors/Providers.java
@@ -3,6 +3,8 @@ package svenhjol.charmony.glint_colors.common.features.glint_colors;
 import svenhjol.charmony.api.glint_colors.GlintColorsApi;
 import svenhjol.charmony.core.base.Setup;
 
+import java.util.Optional;
+
 public class Providers extends Setup<GlintColors> {
     public Providers(GlintColors feature) {
         super(feature);
@@ -11,9 +13,20 @@ public class Providers extends Setup<GlintColors> {
     @Override
     public Runnable boot() {
         return () -> {
-            GlintColorsApi.instance().setApply((i, d) -> feature().handlers.apply(i, d));
-            GlintColorsApi.instance().setRemove(i -> feature().handlers.remove(i));
-            GlintColorsApi.instance().setHas(i -> feature().handlers.has(i));
+            // Implementation to apply a glint color to an item.
+            GlintColorsApi.instance().setApplyImpl((stack, color) -> feature().handlers.apply(stack, color));
+
+            // Implementation to remove a glint color from an item.
+            GlintColorsApi.instance().setRemoveImpl(stack -> feature().handlers.remove(stack));
+
+            // Implementation to check if an item has a glint color.
+            GlintColorsApi.instance().setHasImpl(stack -> feature().handlers.has(stack));
+
+            // Implementation to get the color of an item. If not present, returns empty optional.
+            GlintColorsApi.instance().setGetColorImpl(stack -> {
+                var color = feature().handlers.get(stack);
+                return Optional.ofNullable(color);
+            });
         };
     }
 }
