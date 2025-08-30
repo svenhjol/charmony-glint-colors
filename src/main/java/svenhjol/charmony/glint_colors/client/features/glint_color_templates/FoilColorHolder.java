b96c3ff399cbbf4331d9f23ef6ce0bb83105f5b9
Sven H <sven.hjol@protonmail.com>
2025-08-30 16:33:50 +0000

    Stupid trident code is stupid

--- src/main/java/svenhjol/charmony/glint_colors/client/features/glint_color_templates/FoilColorHolder.java
+++
@@ -1,7 +0,0 @@
-package svenhjol.charmony.glint_colors.client.features.glint_color_templates;
-
-import net.minecraft.world.item.ItemStack;
-
-public interface FoilColorHolder {
-    void setFoilColorFromItemStack(ItemStack stack);
-}
---
+++ src/main/java/svenhjol/charmony/glint_colors/common/mixins/CommonMixinConfig.java
@@ -0,0 +1,22 @@
+package svenhjol.charmony.glint_colors.common.mixins;
+
+import svenhjol.charmony.api.core.Side;
+import svenhjol.charmony.core.base.MixinConfig;
+import svenhjol.charmony.glint_colors.GlintColorsMod;
+
+public class CommonMixinConfig extends MixinConfig {
+    @Override
+    protected String modId() {
+        return GlintColorsMod.ID;
+    }
+
+    @Override
+    protected String modRoot() {
+        return "svenhjol.charmony.glint_colors";
+    }
+
+    @Override
+    protected Side side() {
+        return Side.Common;
+    }
+}
